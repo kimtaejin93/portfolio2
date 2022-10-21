903c5884ec236a81882718d730f97b943f84a0f6
Taejin Kim <tj4467@naver.com>
2022-10-21 23:02:03 +0900

    working

--- src/component/Page1.js
+++ src/component/Page1.js
@@ -6,7 +6,9 @@ const Page1 = () => {
     <Container>
         <div>
             <Row className='row-style'>
-                <Col md={8}>
+                <Col md={4}>
+                </Col>
+                <Col md={4}>
                     <div className='name'>
                         Taejin Kim
                     </div>
@@ -14,8 +16,8 @@ const Page1 = () => {
                         개발자
                     </div>
                 </Col>
-                <Col md={4}>
-                    <div className='subject'>
+                <Col md={4} className='subject'>
+                    <div>
                         PORTFOLIO
                     </div>
                     
@@ -25,22 +27,58 @@ const Page1 = () => {
             </Row>
         </div>
         <div>
+        
+            <div className='line-deco'>     
+            </div>
+            <div className='line-deco2'>
+                introduce
+            </div>
+            <Row className='introduce'>
+            <Col md={3}>
+                <div className='tb-1'>IT'S ME</div>
+            </Col>
+            <Col md={9}>
+                <div className='tb-1-1'>Kongju university graduated</div>
+                <Row>
+                    <Col md={4}>
+                        <div className='tb-1-2'>
+                        <h6>제목</h6>
+                            <p>내용</p>
+                        </div>
+                    </Col>
+                    <Col md={4}>
+                        <div className='tb-1-2'>
+                        <h6>제목</h6>
+                            <p>내용</p>
+                        </div>
+                    </Col>
+                    <Col md={4}>
+                        <div className='tb-1-2'>
+                            <h6>제목</h6>
+                            <p>내용</p>
+                        </div>
+                    </Col>
+                </Row>
+
+                
+            </Col>
+        </Row>
         <Row>
-                <Col md={8}>
-                    <div className='box'>
-                        Taejin Kim
-                    </div>
-                    <div className='box'>
-                        프론트엔드 개발자s
-                    </div>
-                </Col>
-                <Col md={4}>
-                    <div className='box'>
-                        KTJ PPT
-                    </div>
-                    
-                </Col>
-            </Row>
+            <Col md={3}>강조할거</Col>
+            <Col md={9}>
+                <Row>
+                    <Col md={4}>2</Col>
+                    <Col md={4}>3</Col>
+                    <Col md={4}>4</Col>
+                </Row>
+                <Row>
+                    <Col md={4}>2</Col>
+                    <Col md={4}>3</Col>
+                    <Col md={4}>4</Col>
+                </Row>
+            </Col>
+        </Row>
+        
         </div>
     </Container>
   )
